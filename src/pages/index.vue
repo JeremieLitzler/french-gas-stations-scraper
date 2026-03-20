05022b6b82506a03fd13a6ce7230c356f10c615d
Jérémie Litzler <jeremie.litzler@gmail.com>
2026-03-20 16:16:19 +0100

    fix: update title and description

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col w-full">
     <h1 class="text-2xl font-bold mb-4 text-center">Coup de pompe</h1>
+    <h2 class="text-xl font-bold mb-4 text-center">
+      Comparez les prix des carburants dans vos stations préférées.
+    </h2>
     <StationPrices />
     <StationManager />
   </div>
